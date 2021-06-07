74085ad5c91bf7b44de7dfb7cebe55c7b51dbab1
EduardoV <plaloq@outlook.com>
2021-06-07 09:23:48 -0500

    Ejercicio 1

---
+++ src/main/java/uaslp/objetos/figuras/PoligonoRegular.java
@@ -0,0 +1,35 @@
+package uaslp.objetos.figuras;
+
+public class PoligonoRegular {
+    private double lado;
+    private double area;
+    private int numeroDeLados;
+
+    public PoligonoRegular(int numeroDeLados, double lado){
+        this.numeroDeLados=numeroDeLados;
+        this.lado=lado;
+    }
+
+    public PoligonoRegular(int numeroDeLados){
+        this.numeroDeLados=numeroDeLados;
+    }
+
+    public void setLado(double lado){
+        this.lado=lado;
+    }
+
+    public double getLado() {
+        return lado;
+    }
+
+    public double getArea(){
+        double perimetro;
+        double apotema;
+        double angulo;
+        perimetro=lado*numeroDeLados;
+        angulo=360/(2*numeroDeLados);
+        apotema=lado/(2*Math.tan(Math.toRadians(angulo)));
+        area=perimetro*apotema/2;
+        return area;
+    }
+}
